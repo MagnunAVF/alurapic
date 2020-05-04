2a1f242de5129c9dc9a8bd95d59678eec2667102
Magnun A V F <magnunavf@gmail.com>
2020-05-04 20:39:45 -0300

    Create Photo List resolver to load data before Component init

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
-import { PhotoService } from '../photo/photo.service';
 import { Photo } from '../photo/photo';
 
 @Component({
@@ -11,17 +10,13 @@ import { Photo } from '../photo/photo';
 })
 export class PhotoListComponent implements OnInit {
   photos: Photo[] = [];
-  filter: string = '';
+  filter = '';
 
   constructor(
-    private photoService: PhotoService,
     private activatedRoute: ActivatedRoute
   ) {}
 
   ngOnInit(): void {
-    const userName = this.activatedRoute.snapshot.params.userName;
-    this.photoService
-      .listFromUser(userName)
-      .subscribe(photos => this.photos = photos);
+    this.photos = this.activatedRoute.snapshot.data.photos;
   }
 }
