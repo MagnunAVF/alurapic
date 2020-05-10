729746f9b11cd3989eba6706fe9a8fde5229b638
Magnun A V F <magnunavf@gmail.com>
2020-05-10 18:34:56 -0300

    Create Service to authenticate user

--- src/app/home/signin/signin.component.ts
+++ src/app/home/signin/signin.component.ts
@@ -1,18 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { AuthService } from 'src/app/core/auth.service';
 
 @Component({
     templateUrl: './signin.component.html'
 })
 export class SignInComponent implements OnInit {
     loginForm: FormGroup;
+    @ViewChild('userNameInput') userNameInput: ElementRef<HTMLInputElement>;
 
-    constructor(private formBuilder: FormBuilder) {}
+    constructor(
+        private formBuilder: FormBuilder,
+        private authService: AuthService,
+        private router: Router
+    ) {}
 
     ngOnInit(): void {
         this.loginForm = this.formBuilder.group({
             userName: ['', Validators.required],
             password: ['', Validators.required]
         });
+    }
+
+    login() {
+        const userName = this.loginForm.get('userName').value;
+        const password = this.loginForm.get('password').value;
+
+        this.authService.authenticate(userName, password)
+            .subscribe(
+                () => this.router.navigate(['user', userName]),
+                err => {
+                    console.log('Error in login');
+                    this.userNameInput.nativeElement.focus();
+                    this.loginForm.reset();
+                }
+            );
     }
 }
