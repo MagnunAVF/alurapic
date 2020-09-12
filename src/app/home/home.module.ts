7d59cac28bf605894c1b4a59fc4fc6e7ff16b59f
Magnun A V F <magnunavf@gmail.com>
2020-09-12 02:03:45 -0300

    Create Signup Component with initial validations

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 import { SignInComponent } from './signin/signin.component';
 import { VMessageModule } from '../shared/components/vmessage/vmessage.module';
+import { SignUpComponent } from './signup/signup.component';
 
 @NgModule({
-    declarations: [ SignInComponent ],
+    declarations: [
+        SignInComponent,
+        SignUpComponent
+    ],
     imports: [
         CommonModule,
+        FormsModule,
         ReactiveFormsModule,
         VMessageModule,
         RouterModule
