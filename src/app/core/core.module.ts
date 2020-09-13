625e38d4fc3ae68932ee6d6dad34299039f82624
Magnun A V F <magnunavf@gmail.com>
2020-09-12 21:03:02 -0300

    Add token to authenticated users requests in backend

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
 import { HeaderComponent } from './header/header.component';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { RequestInterceptor } from './auth/request.interceptor';
 
 @NgModule({
     declarations: [HeaderComponent],
@@ -10,6 +12,13 @@ import { HeaderComponent } from './header/header.component';
     imports: [
         CommonModule,
         RouterModule
+    ],
+    providers: [
+      {
+        provide: HTTP_INTERCEPTORS,
+        useClass: RequestInterceptor,
+        multi: true
+      }
     ]
 })
 export class CoreModule { }
