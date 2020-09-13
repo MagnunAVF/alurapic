bbf0aa61d00c9a20b53b2f7f81d5efd09e2c05b1
Magnun A V F <magnunavf@gmail.com>
2020-09-13 00:38:23 -0300

    Create Immediate Click Directive
    - Use it in Add new Photo.

--- src/app/shared/directives/darken-on-hover/darken-on-hover.module.ts
+++ src/app/shared/directives/darken-on-hover/darken-on-hover.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 import { DarkOnHoverDirective } from './darken-on-hover.directive';
 
 @NgModule({
     declarations: [ DarkOnHoverDirective ],
-    exports: [ DarkOnHoverDirective ]
+    exports: [ DarkOnHoverDirective ],
+    imports: [ CommonModule ]
 })
 export class DarkenOnHoverModule {}
